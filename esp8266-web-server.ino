b55db4904c254379cf42ddc3b216e7d1720ca5eb
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-02-20 22:57:46 +0530

    feat: set up esp8266 to connect to wifi

--- esp8266-web-server.ino
+++ esp8266-web-server.ino
@@ -1,7 +1,36 @@
-void setup() {
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
 
+#define WIFI_SSID "My_Wi-Fi"
+#define WIFI_PASSWORD "my-awesome-password"
+
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
+Ticker wifiReconnectTimer;
+
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void onWifiConnect(const WiFiEventStationModeGotIP& event) {
+  Serial.println("Connected to Wi-Fi.");
+}
+
+void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
+  Serial.println("Disconnected from Wi-Fi.");
+  wifiReconnectTimer.once(2, connectToWifi);
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+
+  connectToWifi();
 }
 
 void loop() {
-  
 }
