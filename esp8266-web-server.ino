8881ec8b3318c548e0e016c70e484e6201b26d5e
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-02-21 09:21:03 +0530

    refactor: apply clang linting

--- esp8266-web-server.ino
+++ esp8266-web-server.ino
@@ -1,6 +1,6 @@
+#include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
 #include <Ticker.h>
-#include <ESP8266WebServer.h>
 
 #define WIFI_SSID "My_Wi-Fi"
 #define WIFI_PASSWORD "my-awesome-password"
@@ -9,56 +9,53 @@ WiFiEventHandler wifiConnectHandler;
 WiFiEventHandler wifiDisconnectHandler;
 Ticker wifiReconnectTimer;
 
-// initialise server at port 80
-ESP8266WebServer server(80);
+// initialise server at port 8266
+ESP8266WebServer server(8266);
 
 void connectToWifi() {
-  Serial.println("Connecting to Wi-Fi...");
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.println("Connecting to Wi-Fi...");
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 }
 
-void onWifiConnect(const WiFiEventStationModeGotIP& event) {
-  Serial.println("Connected to Wi-Fi.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+void onWifiConnect(const WiFiEventStationModeGotIP &event) {
+    Serial.println("Connected to Wi-Fi.");
+    Serial.println("Local IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
-void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
-  Serial.println("Disconnected from Wi-Fi.");
-  wifiReconnectTimer.once(2, connectToWifi);
+void onWifiDisconnect(const WiFiEventStationModeDisconnected &event) {
+    Serial.println("Disconnected from Wi-Fi.");
+    wifiReconnectTimer.once(2, connectToWifi);
 }
 
 String generateRootHTML() {
-  String pageContent = "<!DOCTYPE html>\n";
-  pageContent += "<html lang=\"en\">\n";
-  pageContent += "<head>\n<meta charset=\"UTF-8\">\n";
-  pageContent += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n";
-  pageContent += "<title>ESP8266 Web Server</title>\n</head>\n";
-  pageContent += "<body>\n<p>Hello World :)</p>\n</body>\n</html>";
+    String pageContent = "<!DOCTYPE html>\n";
+    pageContent += "<html lang=\"en\">\n";
+    pageContent += "<head>\n<meta charset=\"UTF-8\">\n";
+    pageContent += "<meta name=\"viewport\" content=\"width=device-width, "
+                   "initial-scale=1.0\">\n";
+    pageContent += "<title>ESP8266 Web Server</title>\n</head>\n";
+    pageContent += "<body>\n<p>Hello World :)</p>\n</body>\n</html>";
 
-  return pageContent;
+    return pageContent;
 }
 
-void getRootPath() {
-  server.send(200, "text/html", generateRootHTML());
-}
+void getRootPath() { server.send(200, "text/html", generateRootHTML()); }
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println();
+    Serial.begin(115200);
+    Serial.println();
 
-  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
-  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
-  connectToWifi();
+    wifiConnectHandler    = WiFi.onStationModeGotIP(onWifiConnect);
+    wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+    connectToWifi();
 
-  delay(100);
+    delay(100);
 
-  server.on("/", getRootPath);
+    server.on("/", getRootPath);
 
-  server.begin();
-  Serial.println("HTTP server started");
+    server.begin();
+    Serial.println("HTTP server started");
 }
 
-void loop() {
-  server.handleClient();
-}
+void loop() { server.handleClient(); }
